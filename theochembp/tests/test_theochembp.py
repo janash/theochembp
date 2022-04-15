45020be3353bdc1c0e914d4f40e5085d97e2f8c2
Jessica Nash <janash@vt.edu>
2022-04-15 17:13:17 +0000

    test calculate_distance function

--- theochembp/tests/test_theochembp.py
+++ theochembp/tests/test_theochembp.py
@@ -6,6 +6,7 @@ Unit and regression test for the theochembp package.
 import sys
 
 import pytest
+import numpy as np
 
 import theochembp
 
@@ -13,3 +14,15 @@ import theochembp
 def test_theochembp_imported():
     """Sample test, will always pass so long as import statement worked."""
     assert "theochembp" in sys.modules
+
+@pytest.mark.skip
+def test_calculate_distance():
+
+    r1 = np.array([0, 0, 0])
+    r2 = np.array([0, 0.1, 0])
+
+    expected_distance = 0.1
+
+    calculated_distance = theochembp.calculate_distance(r1, r2)
+
+    assert pytest.approx(expected_distance) == calculated_distance
