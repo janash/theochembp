43b1dcc38abde680bb9c43ca7df06fdcaff26745
Jessica Nash <janash@vt.edu>
2022-04-15 18:02:06 +0000

    update tests

--- theochembp/tests/test_theochembp.py
+++ theochembp/tests/test_theochembp.py
@@ -15,7 +15,6 @@ def test_theochembp_imported():
     """Sample test, will always pass so long as import statement worked."""
     assert "theochembp" in sys.modules
 
-@pytest.mark.skip
 def test_calculate_distance():
 
     r1 = np.array([0, 0, 0])
@@ -26,3 +25,30 @@ def test_calculate_distance():
     calculated_distance = theochembp.calculate_distance(r1, r2)
 
     assert pytest.approx(expected_distance) == calculated_distance
+    
+# Exercise - write a test for the calculate_angle function.
+
+def test_calculate_angle():
+    """Test the calculate angle function"""
+
+    r1 = np.array([1, 0, 0])
+    r2 = np.array([0, 0, 0])
+    r3 = np.array([0, 1, 0])
+
+    expected_value = 90
+
+    calculated_value = theochembp.calculate_angle(r1, r2, r3, degrees=True)
+
+    assert pytest.approx(expected_value) == calculated_value
+
+@pytest.mark.parametrize("p1, p2, p3, expected_angle",[
+    (np.array([np.sqrt(2)/2, np.sqrt(2)/2, 0]), np.array([0, 0, 0]), np.array([1, 0, 0]), 45),
+    (np.array([0, 0, -1]), np.array([0, 1, 0]), np.array([1, 0, 0]), 60)
+])
+def test_calculate_angle_many(p1, p2, p3, expected_angle):
+
+    calculated_angle = theochembp.calculate_angle(p1, p2, p3, degrees=True)
+
+    assert pytest.approx(expected_angle) == calculated_angle
+
+
